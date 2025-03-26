fc8ebdce36d93fe10cfb956922fd88aef99fbfe5
4ec <4echoko@googlemail.com>
2025-03-26 12:32:02 +0300

    fix: Дипломный проект. Этап 1
    - fix билдера

--- core/main-service/src/main/java/ru/practicum/controller/params/search/AdminSearchParams.java
+++ core/main-service/src/main/java/ru/practicum/controller/params/search/AdminSearchParams.java
@@ -1,9 +1,6 @@
 package ru.practicum.controller.params.search;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import ru.practicum.entity.EventState;
 
 import java.time.LocalDateTime;
@@ -13,6 +10,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @Getter
 @Builder
+@AllArgsConstructor
 public class AdminSearchParams {
 
     private List<Long> users;
